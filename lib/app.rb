eb23905c67c9f26bb5e97e818eaea100c3734a54
Mic <mic.cassano@gmail.com>
2016-02-01 12:29:43 +0000

    Add HTML return

--- lib/app.rb
+++ lib/app.rb
@@ -2,13 +2,17 @@ require 'sinatra'
 require 'shotgun'
 
 get '/' do
-  "Come fly with me, come fly, let's fly away.."
+  'Come fly with me, come fly, let\'s fly away..'
 end
 
 get '/secret' do
-  "yoyoyo"
+  'yoyoyo'
 end
 
 get '/another' do
-  "can you read me?"
+  'can you read me?'
+end
+
+get '/cat' do
+    "<img src='http://bit.ly/1eze8aE' alt='cat' style='border: medium dashed red;'>"
 end
