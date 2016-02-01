c882f3b64c011df9133239f1150283912883d7a1
Henry Gordon <hwgordon247@gmail.com>
2016-02-01 12:19:06 +0000

    adding of shotgun gem

--- lib/app.rb
+++ lib/app.rb
@@ -1,5 +1,14 @@
 require 'sinatra'
+require 'shotgun'
 
 get '/' do
   "Come fly with me, come fly, let's fly away.."
+end
+
+get '/secret' do
+  "yoyoyo"
+end
+
+get '/another' do
+  "can you read me?"
 end
